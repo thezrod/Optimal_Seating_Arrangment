a599eb853a12fa8f8a1e5b057e613a669fe74871
thezrod <thezrod_93@live.se>
2024-02-01 11:53:55 +0100

    Solved the final parts of the task. I did not manage to find the perfect/optimal solution but all the parts of the task is present and the people are seated and the score is calculated.

--- src/Calculate_Happiness.java
+++ src/Calculate_Happiness.java
@@ -19,16 +19,9 @@ public class Calculate_Happiness {
         Person p1 = People.getPerson(personOne);
         Person p2 = People.getPerson(personTwo);
 
-        /*
-        System.out.println("----------------------");
-        System.out.println("Happiness Between:");
-        System.out.println(p1.name + " & " + p2.name);
-*/
         totalHappiness += p1.getHappinessScoreByName(p2.name);
         totalHappiness += p2.getHappinessScoreByName(p1.name);
 
-       /* System.out.println("Total Happiness: " + totalHappiness);
-        System.out.println("----------------------" + "\n");*/
         return totalHappiness;
     }
 
@@ -54,6 +47,28 @@ public class Calculate_Happiness {
             }
         }
             return maximumHappiness;
+    }
+    /*
+     * Takes a string representing a Persons name and a list of Persons.
+     * Returns the highest total score for a person from the list
+     *
+     * */
+    static protected int findHighestTotal(String person, ArrayList<Person> otherPeople) {
+        int maximumHappiness = 0;
+        ArrayList<Pair> totalHappinessWithPerson = new ArrayList<Pair>();
+
+        for (Person p : otherPeople) {
+            if (!p.name.equals(person)) {
+                totalHappinessWithPerson.add(new Pair<>(p.name, calcHappinessBetweenTwo(person, p.name)));
+            }
+        }
+
+        for (Pair p : totalHappinessWithPerson) {
+            if ((int) p.getValue() > maximumHappiness) {
+                maximumHappiness = (int) p.getValue();
+            }
+        }
+        return maximumHappiness;
     }
 
     /*
@@ -101,107 +116,6 @@ public class Calculate_Happiness {
         return highestScorer;
     }
 
-    static protected ArrayList<String> getNameOfXHighestScorer(String person, int numberOfPeople, ArrayList<Person> otherPeople){
-        ArrayList<String> highscoreList = new ArrayList<>();
-        if(numberOfPeople > otherPeople.size()){
-            highscoreList.add("To many people wanted");
-            return highscoreList;
-        }
-
-        for(int i =0; i < numberOfPeople; i++) {
-            System.out.println("Loop #" + (i+1));
-            int maximumHappiness = 0;
-            String highestScorer = "";
-            ArrayList<Pair> totalHappinessWithPerson = new ArrayList<Pair>();
-
-            for (Person p : otherPeople) {
-                if (!p.name.equals(person)) {
-                    totalHappinessWithPerson.add(new Pair<>(p.name, calcHappinessBetweenTwo(person, p.name)));
-                }
-            }
-
-            Person tempPerson = new Person("");
-            for (Pair p : totalHappinessWithPerson) {
-                if ((int) p.getValue() > maximumHappiness) {
-                    maximumHappiness = (int) p.getValue();
-                    highestScorer = (String) p.getKey();
-                    System.out.println("Highscorer found: " + highestScorer);
-                    highscoreList.add(highestScorer);
-                    System.out.println("highscoreList updated: " + highscoreList);
-                    tempPerson.setPerson(People.getPerson((String)p.getKey()));
-                }
-            }
-            otherPeople.remove(tempPerson);
-            System.out.println("List of people updated " + otherPeople);
-        }
-        return highscoreList;
-    }
-
-
-    static protected int findHighestTotalFromRemaining(String person, ArrayList<Person> remaining) {
-        int maximumHappiness = 0;
-        ArrayList<Pair> totalHappinessWithPerson = new ArrayList<Pair>();
-
-        for (Person p : remaining) {
-            if (!p.name.equals(person)) {
-                totalHappinessWithPerson.add(new Pair<>(p.name, calcHappinessBetweenTwo(person, p.name)));
-            }
-        }
-
-        for (Pair p : totalHappinessWithPerson) {
-            if ((int) p.getValue() > maximumHappiness) {
-                maximumHappiness = (int) p.getValue();
-            }
-        }
-        return maximumHappiness;
-    }
-
-
-    static protected String getHighestScorerFromRemaining(String person, ArrayList<Person> remaining){
-        int maximumHappiness = 0;
-        String highestScorer = "";
-        ArrayList<Pair> totalHappinessWithPerson = new ArrayList<Pair>();
-
-        for (Person p : remaining) {
-            if (!p.name.equals(person)) {
-                totalHappinessWithPerson.add(new Pair<>(p.name, calcHappinessBetweenTwo(person, p.name)));
-            }
-        }
-
-        for (Pair p : totalHappinessWithPerson) {
-            if ((int) p.getValue() > maximumHappiness) {
-                maximumHappiness = (int) p.getValue();
-                highestScorer = (String)p.getKey();
-            }
-        }
-        return highestScorer;
-    }
-
-    static protected String getHighestScorerExcluding(String person, ArrayList<Person> exclusions){
-        int maximumHappiness = 0;
-        String highestScorer = "";
-        ArrayList<Pair> totalHappinessWithPerson = new ArrayList<Pair>();
-
-        for (Person p : People.persons) {
-            if (!p.name.equals(person)) {
-                for (Person p2:exclusions){
-                    if(!p.equals(p2)){
-                        totalHappinessWithPerson.add(new Pair<>(p.name, calcHappinessBetweenTwo(person, p.name)));
-                    }
-
-                }
-
-            }
-        }
-
-        for (Pair p : totalHappinessWithPerson) {
-            if ((int) p.getValue() > maximumHappiness) {
-                maximumHappiness = (int) p.getValue();
-                highestScorer = (String)p.getKey();
-            }
-        }
-        return highestScorer;
-    }
     static protected ArrayList<Person> getHighestScoringPair(){
         int bestScoreSoFar = 0;
         ArrayList<Person> highsocrePair = new ArrayList<>();
@@ -220,38 +134,5 @@ public class Calculate_Happiness {
             }
         }
         return highsocrePair;
-    }
-
-    static int calculateTotalHappinessOfTable(String[] seating){
-        int totalHappiness = 0;
-        System.out.println("seating.length: " + seating.length);
-        for (int i = 0; i < seating.length; i++){
-
-            if(i == 0){
-                totalHappiness += Calculate_Happiness.calcHappinessBetweenTwo(People.getPerson(seating[i]).name, People.getPerson(seating[seating.length-1]).name);
-                totalHappiness += Calculate_Happiness.calcHappinessBetweenTwo(People.getPerson(seating[i]).name, People.getPerson(seating[i+1]).name);
-            }
-             else if(i == (seating.length-1)){
-                totalHappiness += Calculate_Happiness.calcHappinessBetweenTwo(People.getPerson(seating[i]).name, People.getPerson(seating[i-1]).name);
-                totalHappiness += Calculate_Happiness.calcHappinessBetweenTwo(People.getPerson(seating[i]).name, People.getPerson(seating[0]).name);
-            }
-            else{
-                totalHappiness += Calculate_Happiness.calcHappinessBetweenTwo(People.getPerson(seating[i]).name, People.getPerson(seating[i-1]).name);
-                totalHappiness += Calculate_Happiness.calcHappinessBetweenTwo(People.getPerson(seating[i]).name, People.getPerson(seating[i+1]).name);
-            }
-
-        }
-        System.out.print("Total Score For Seating" + stringList(seating) + ": ");
-        return totalHappiness;
-    }
-
-    static String stringList(String[] list){
-        String output = " {";
-
-        for (String s:list) {
-            output += s+ ", ";
-        }
-        output += "}";
-        return output;
     }
 }
--- src/Table.java
+++ src/Table.java
@@ -73,272 +73,43 @@ public class Table {
         return true;
     }
 
-    /*"Stupid" seating that just seats the people around the table not using any algorithm to calculate happiness*/
-    protected static void seatAllPeople(){
-        int currentSeat = 0;
-        for (Person p: People.persons) {
-            seatPerson(p, currentSeat);
-            currentSeat++;
+
+    protected int seatAndScorePeople(){
+        int totalScore = 0;
+        ArrayList<Person> copyPersons = People.persons;
+        ArrayList<Person> startingPair = Calculate_Happiness.getHighestScoringPair();
+
+        int seatsLeft = getNumSeats();
+
+        int nextSeat = 0;
+
+        for (Person p: startingPair) {
+            seatPerson(p, nextSeat);
+            nextSeat++;
+            seatsLeft--;
+            System.out.println("Setting seats");
         }
+
+        totalScore += Calculate_Happiness.calcHappinessBetweenTwo(startingPair.get(0).name, startingPair.get(1).name);
+        copyPersons.remove(startingPair.get(0));
+
+
+        while (seatsLeft > 0){
+            Person currentPerson = People.getPerson(getSeatedAt(nextSeat-1));
+            Person newNeighbor = People.getPerson(Calculate_Happiness.getNameOfHighestScorer(currentPerson.name, copyPersons));
+            seatPerson(newNeighbor, nextSeat);
+            totalScore += (Calculate_Happiness.findHighestTotal(currentPerson.name, copyPersons));
+            copyPersons.remove(currentPerson);
+            nextSeat++;
+            seatsLeft--;
+        }
+        System.out.println("Final totalScore: " + totalScore +"\n");
+
+        System.out.print("Final table: \n" + "{ ");
+        for (String seat: seats) {
+            System.out.print(seat + ", ");
+        }
+        System.out.println("}");
+        return totalScore;
     }
-    /*Seats all people, starting in alphabetical order and finding the best neighbours*/
-    protected static void AlphabeticalHappinessSeating(){
-        ArrayList<Integer> availableSeats = new ArrayList<>();
-        for (int i = 0; i < seats.size(); i++){
-            availableSeats.add(i);
-            System.out.println("availableSeats[" + i + "] added");
-        }
-        ArrayList<Person> nonSeatedPeople = People.persons;
-
-        ArrayList<Person> seatedPeople = new ArrayList<>();
-        String highestScorer;
-        String secondHighestScorer;
-
-
-        for (int i = 1; i < nonSeatedPeople.size()-1; i ++) {
-            if(nonSeatedPeople.size() == 1 && availableSeats.size() == 1){
-                seatPerson(nonSeatedPeople.get(0), availableSeats.get(0));
-            }
-            else if(nonSeatedPeople.size() == 2){
-
-            }
-            else {
-                Person currentPerson = nonSeatedPeople.get(0);
-                seatPerson(currentPerson, availableSeats.get(0));
-                highestScorer = Calculate_Happiness.getNameOfHighestScorer(currentPerson.name);
-                nonSeatedPeople.remove(highestScorer);
-                secondHighestScorer = Calculate_Happiness.getHighestScorerFromRemaining(currentPerson.name, nonSeatedPeople);
-
-                seatedPeople.add(People.getPerson(highestScorer));
-                secondHighestScorer = Calculate_Happiness.getHighestScorerExcluding(currentPerson.name, seatedPeople);
-
-                seatPerson(People.getPerson(highestScorer), getNeighbourSeats(availableSeats.get(0)).get(0));
-                seatPerson(People.getPerson(secondHighestScorer), getNeighbourSeats(availableSeats.get(0)).get(1));
-                nonSeatedPeople.remove(0);
-            }
-            if(availableSeats.size() > 3) {
-                availableSeats.remove(0);
-                availableSeats.remove(getNeighbourSeats(availableSeats.get(0)).get(0));
-                availableSeats.remove(getNeighbourSeats(availableSeats.get(0)).get(1));
-            }
-        }
-    }
-
-    protected static void Alpha(String name){
-        ArrayList<Integer> availableSeats = new ArrayList<>();
-        for (int i = 0; i < seats.size(); i++){
-            availableSeats.add(i);
-        }
-        ArrayList<Person> seatedPeople = new ArrayList<>();
-
-
-
-        String highestScorer;
-        String secondHighestScorer;
-
-            Person currentPerson = People.getPerson(name);
-            seatPerson(currentPerson, availableSeats.get(0));
-            highestScorer = Calculate_Happiness.getNameOfHighestScorer(currentPerson.name);
-            seatedPeople.add(People.getPerson(highestScorer));
-            secondHighestScorer = Calculate_Happiness.getHighestScorerExcluding(currentPerson.name, seatedPeople);
-
-            System.out.println("highestScorer" +Calculate_Happiness.getHighestScorerFromRemaining(currentPerson.name, People.persons));
-            System.out.println( "nonseatedPeople.remove: " + People.persons.remove(highestScorer));
-            System.out.println( "secondHighestScorer: " + Calculate_Happiness.getHighestScorerFromRemaining(currentPerson.name, People.persons));
-
-            System.out.println("Neighbours: ");
-            System.out.println(getNeighbourSeats(availableSeats.get(0)).get(0));
-            System.out.println(getNeighbourSeats(availableSeats.get(0)).get(1));
-
-            seatPerson(People.getPerson(highestScorer), getNeighbourSeats(availableSeats.get(0)).get(0));
-            seatPerson(People.getPerson(secondHighestScorer), getNeighbourSeats(availableSeats.get(0)).get(1));
-    }
-
-    protected static void SequentialSeating(){
-        ArrayList<Integer> availableSeats = new ArrayList<>();
-        for (int i = 0; i < seats.size(); i++){
-            availableSeats.add(i);
-            System.out.println("availableSeats[" + i + "] added");
-        }
-        ArrayList<Person> nonSeatedPeople = People.persons;
-        ArrayList<Person> seatedPeople = new ArrayList<>();
-        String highestScorer;
-
-
-        for (int i = 1; i < nonSeatedPeople.size(); i ++) {
-        Person currentPerson = nonSeatedPeople.get(0);
-            for(int j = 0; j < availableSeats.size(); j++) {
-                if(seatPerson(currentPerson, availableSeats.get(j))) {
-                    seatedPeople.add(currentPerson);
-
-                    highestScorer = Calculate_Happiness.getHighestScorerExcluding(currentPerson.name, seatedPeople);
-                    boolean success = false;
-                    for(int k = 0; k < 2; k++){
-                        if(seatPerson(People.getPerson(highestScorer), getNeighbourSeats(availableSeats.get(j)).get(k))) {
-                            seatedPeople.add(People.getPerson(highestScorer));
-                            nonSeatedPeople.remove(highestScorer);
-
-                            availableSeats.remove(0);
-                            availableSeats.remove(getNeighbourSeats(availableSeats.get(0)).get(0));
-                            success = true;
-                            break;
-                        }
-                    }
-                    if(!success){
-                        System.out.println("No space!");
-                    }
-
-                    nonSeatedPeople.remove(0);
-                break;
-                }
-            }
-        }
-        System.out.println("Seated People: ");
-        for (Person p:seatedPeople) {
-            System.out.print(p.name + ", ");
-        }
-        System.out.println("\n"+"NonSeated People: ");
-        for (Person p:nonSeatedPeople) {
-            System.out.print(p.name + ", ");
-        }
-        System.out.println("\n");
-    }
-
-    /*
-    * Finds and returns the to neighbour-seats of a seat.
-    * */
-    protected static ArrayList<Integer> getNeighbourSeats(int seatNumber){
-        ArrayList<Integer> neighbours = new ArrayList<>();
-        if(seatNumber == 0){
-            neighbours.add(getNumSeats()-1);
-        }
-        else{
-            neighbours.add(seatNumber-1);
-        }
-        if(seatNumber != getNumSeats()-1){
-            neighbours.add(seatNumber+1);
-        }
-        else{
-            neighbours.add(0);
-        }
-
-        return neighbours;
-    }
-
-    protected static void MyNewList(){
-
-        ArrayList<Integer> availableSeats = new ArrayList<>();
-
-        for (int i = 0; i < seats.size(); i++){
-            availableSeats.add(i);
-            System.out.println("availableSeats[" + i + "] added");
-        }
-        int numbAvailSeats = availableSeats.size();
-        ArrayList<Person> nonSeatedPeople = People.persons;
-        ArrayList<Person> seatedPeople = new ArrayList<>();
-        ArrayList<Person> calculatedPeople = new ArrayList<>();
-
-        while(numbAvailSeats > 0) {
-            if (seatedPeople.size() == 0) {
-                seatPerson(nonSeatedPeople.get(0), availableSeats.get(0));
-                seatedPeople.add(nonSeatedPeople.get(0));
-                nonSeatedPeople.remove(0);
-                calculatedPeople.add(nonSeatedPeople.get(0));
-                numbAvailSeats--;
-            }
-            else {
-                for (Person p : seatedPeople) {
-                    for (Person p2 : calculatedPeople) {
-                        if (!p.equals(p2)) {
-                            calculatedPeople.add(p);
-                            String nextPerson = Calculate_Happiness.getNameOfHighestScorer(p.name);
-                            seatPerson(People.getPerson(nextPerson),availableSeats.get(0));
-                            numbAvailSeats--;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        System.out.println("Seated People: ");
-        for (Person p:seatedPeople) {
-            System.out.print(p.name + ", ");
-        }
-        System.out.println("\n"+"NonSeated People: ");
-        for (Person p:nonSeatedPeople) {
-            System.out.print(p.name + ", ");
-        }
-        System.out.println("\n");
-    }
-
-    /**
-     * 1) Generate all combinations of seating.
-     * 2) Calculate each combination
-     * 3) Output the greatest Happiness Score ant the seating arrangement.
-     *
-     * */
-    static void permute(ArrayList<Person> arr, int k){
-
-        for(int i = k; i < arr.size(); i++){
-            java.util.Collections.swap(arr, i, k);
-            permute(arr, k+1);
-            java.util.Collections.swap(arr, k, i);
-
-        }
-        if (k == arr.size() -1){
-            System.out.println(java.util.Arrays.toString(arr.toArray()));
-            
-        }
-    }
-    static void seatPeople(){
-        ArrayList<Person> unseatedPeople = People.persons;
-        ArrayList<Person> seatedPeople = People.persons;
-        int emptySeats = getNumOfEmptySeats();
-        System.out.println("Number of empty seats: " + emptySeats);
-        while(emptySeats > 0){
-            if(emptySeats == People.getNumberOfPeople()) {
-                ArrayList<Person> firstPair =Calculate_Happiness.getHighestScoringPair();
-                for (Person p: firstPair) {
-                    seatPerson(p, getFirstAvailableSeat());
-                    seatedPeople.add(p);
-                    unseatedPeople.remove(p);
-                }
-            }
-            else{
-                //Person tmp = new Person("");
-                for(int i = 0; i < seatedPeople.size(); i++){
-                    if(!seats.get(i).isEmpty()){
-                        for (int position:getNeighbourSeats(i)) {
-                            if(seats.get(position).isEmpty()){
-                                seatPerson(People.getPerson(Calculate_Happiness.getNameOfHighestScorer(seats.get(i),unseatedPeople)), position);
-
-                                // unseatedPeople.remove(People.getPerson(Calculate_Happiness.getNameOfHighestScorer(seats.get(i), unseatedPeople)));
-
-                            }
-                        }
-                    }
-                }
-            }
-            emptySeats = getNumOfEmptySeats();
-            System.out.println("Number of empty seats: " + emptySeats);
-        }
-        System.out.println("Seated People: ");
-        for (Person p:seatedPeople) {
-            System.out.print(p.name + ", ");
-        }
-        System.out.println("\n"+"NonSeated People: ");
-        for (Person p:unseatedPeople) {
-            System.out.print(p.name + ", ");
-        }
-        System.out.println("\n");
-    }
-
-    private static int getFirstAvailableSeat() {
-        for (int i = 0; i < getNumSeats(); i++){
-            if (seats.get(i).isEmpty())
-                return i;
-        }
-        return -1;
-    }
-
 }
